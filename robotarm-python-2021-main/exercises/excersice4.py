d103f517919b39cae4df78ab944258bca1949bb1
Sam Fortuin <99059050@mydavinci.nl>
2021-10-26 11:26:32 +0200

    added beoordelingsformulier

---
+++ robotarm-python-2021-main/exercises/excersice4.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('robotarm-python-2021-main')
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 4')
+
+def robotMoveRight(forMany):
+    for i in range(forMany):robotArm.moveRight()
+
+def robotMoveLeft(forMany2):
+    for i in range(forMany2):robotArm.moveLeft()
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+robotMoveRight(2)
+robotArm.drop()
+robotMoveLeft(2)
+robotArm.grab()
+robotMoveRight(2)
+robotArm.drop()
+robotMoveLeft(2)
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+for i in range(2):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
