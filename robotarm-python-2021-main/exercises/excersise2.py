ad9fae43b3a56368b541ad7859457364f76a109a
Sam Fortuin <99059050@mydavinci.nl>
2021-10-26 11:08:33 +0200

    finsihed exercise 2

---
+++ robotarm-python-2021-main/exercises/excersise2.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('robotarm-python-2021-main')
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 2')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+for i in range(9):robotArm.moveRight()
+robotArm.drop()
+for i in range(5):robotArm.moveLeft()
+robotArm.grab()
+for i in range(5):robotArm.moveRight()
+robotArm.drop()
+for i in range(2):robotArm.moveLeft()
+robotArm.grab()
+for i in range(2):robotArm.moveRight()
+robotArm.drop()
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
