0c12b990e5f5d95f03b924f09470c2006eba3f46
Sam Fortuin <99059050@mydavinci.nl>
2021-10-26 11:40:31 +0200

    finished 6

---
+++ robotarm-python-2021-main/exercises/excercise6.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('robotarm-python-2021-main')
+from RobotArm import RobotArm
+
+def robotMoveRight(forMany):
+    for i in range(forMany):robotArm.moveRight()
+
+def robotMoveLeft(forMany2):
+    for i in range(forMany2):robotArm.moveLeft()
+
+robotArm = RobotArm('exercise 6')
+robotMoveRight(7)
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+for i in range(7):
+    if i == 0:
+        robotArm.moveLeft()
+    else:
+        robotMoveLeft(2)
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+# Jouw python instructies zet je vanaf hier:
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
